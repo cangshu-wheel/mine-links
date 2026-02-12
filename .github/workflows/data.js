1490dedcd168c657b2e546485e9228b91d984ff1
cangshu <43460412+cangshu-wheel@users.noreply.github.com>
2026-02-12 09:25:13 +0800

    Add files via upload

---
+++ .github/workflows/data.js
@@ -0,0 +1,141 @@
+const data = {
+  "sections": [
+    {
+      "title": "看电影看电视",
+      "items": [
+         {
+          "name": "阿虚",
+          "url": "https://axutongxue.com/",
+          "description": "阿虚",
+          "featured": true
+        },  {
+          "name": "下载的影视",
+          "url": "https://seedhub.pro/",
+          "description": "这玩意要下载",
+          "featured": true
+        },
+        {
+          "name": "火花影视",
+          "url": "https://www.kdocs.cn/l/cbcQPkESfLpu?R=L1MvNA%3D%3D",
+          "description": "影视网址",
+          "featured": false
+        }
+      ]
+    },
+    {
+      "title": "游戏",
+      "items": [
+        {
+          "name": "游戏网址",
+          "url": "https://koyso.to/",
+          "description": "游戏网址",
+          "featured": true
+        },
+        {
+              "name": "有点牛的在线摸鱼游戏网址",
+          "url": "  https://www.freeonlinegame.games/zh-CN",
+          "description": "确实有点牛",
+          "featured": true
+        
+        },
+        {
+          "name": "gamer520",
+          "url": "https://www.gamer520.com/",
+          "description": "游戏网址",
+           "featured": false
+        },
+        {
+          "name": "终末地地图资源查询",
+          "url": "https://zmdmap.com",
+          "description": "终末地地图资源查询网站",
+          "featured": true
+        },  {
+          "name": "终末地基质规划器",
+          "url": "https://end.canmoe.com/",
+          "description": "终末地基质规划器",
+          "featured": true
+        },
+        {
+          "name": "FF14log",
+          "url": "https://www.fflogs.com/",
+          "description": "FF14战斗日志分析网站",
+          "featured": false
+        },
+        {
+          "name": "fling游戏下载",
+          "url": "https://flingtrainer.com",
+          "description": "游戏训练器下载网站",
+          "featured": false
+        },
+        {
+          "name": "ff14mod",
+          "url": "https://souma.diemoe.net/ff14-overlay-vue/#/startPages",
+          "description": "FF14 overlay mod工具",
+          "featured": false
+        },
+        {
+          "name": "胜利女神维基百科",
+          "url": "https://www.gamekee.com/nikke/",
+          "description": "NIKKE游戏维基百科",
+          "featured": true
+        },  
+        {
+          "name": "挖矿的",
+          "url": "https://golddigger.frvr.com/",
+          "description": "一款设计精美的挖矿游戏，控制矿工在地下随意挖掘收集物品和黄金，避免被石头砸死",
+          "featured": false
+        },
+          {
+          "name": "YXMB",
+          "url": "https://yxmb.space/",
+          "description": "小众游戏",
+          "featured": false
+        },
+        {
+          "name": "galagame",
+          "url": " https://www.kungal.com/",
+          "description": "旮旯Game",
+          "featured": false
+     
+        },
+        {
+          "name": "在线迷宫网址",
+          "url": "https://maze.toys/",
+          "description": "在线玩简单的迷宫，有多种模式，如普通、争分夺秒等",
+          "featured": false
+        }
+      ]
+    },
+    {
+      "title": "动漫",
+      "items": [
+        {
+          "name": "omo动漫网址",
+          "url": "https://www.omofuna.com/",
+          "description": "动漫资源网站",
+          "featured": true
+        },{
+          "name":'pix',
+          "url": "https://pixiv.pictures/",
+          "description": "图片壁纸",
+          
+        }
+      ]
+    },
+   
+    {
+      "title": "其他",
+      "items": [
+        {
+          "name": "资讯网",
+          "url": "https://tgmeng.com/",
+          "description": "看新闻用的",
+            "featured": true
+        },
+      
+      
+      ]
+    },
+      
+  ]
+};
